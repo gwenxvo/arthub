5e6871ca259aecfd3993954d04b48d52a95069c1
Monika Vo <monika-quyen@hotmail.de>
2023-06-05 17:13:37 +0200

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,10 @@
 puts 'Cleaning database..'
 Artpiece.destroy_all
 
+User.create(email: "test@lewagon.com", password: "password", password_confirmation: "password")
+
 puts 'Adding movies..'
-@artpiece = Artpiece.new(title: "Water Lillies", description: "Huge painting, 300cm x 180cm", artist: "Claude Monet", day_price: 75)
-Artpiece.create(title: "Starry Night", description: "Classic artpiece, 120cm x 70cm", artist: "Vincent van Gogh", day_price: 122)
+Artpiece.create(title: "Water Lillies", description: "Huge painting, 300cm x 180cm", artist: "Claude Monet", day_price: 75, user: User.last)
+Artpiece.create(title: "Starry Night", description: "Classic artpiece, 120cm x 70cm", artist: "Vincent van Gogh", day_price: 122, user: User.first)
 
 puts 'Finished..'
