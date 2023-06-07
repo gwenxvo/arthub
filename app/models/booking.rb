5b4d0dabcbe94a723562e300eb8d7af3d4a31384
Rostyslav Komar <komarostyslav@gmail.com>
2023-06-07 12:33:48 +0300

    booking logic

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,20 +3,14 @@ class Booking < ApplicationRecord
   belongs_to :artpiece
 
   validates :start_date, :end_date, presence: true
-  validate :end_date_after_start_date
-  validate :booking_check
+  validate :dates_do_not_overlap
 
   private
 
-  def end_date_after_start_date
-    return unless start_date && end_date
-
-    errors.add(:end_date, "must be after start date") if end_date <= start_date
-  end
-
-  def booking_check
-    if Booking.where("artpiece_id = ? and ? < end_date and ? > start_date", artpiece_id, start_date, end_date).any?
-      errors.add(:base, "That fancy-schmancy artpiece you are eyeing is it's totally locked and loaded for that specific period")
-    end
+  def dates_do_not_overlap
+    overlaps = Booking.where(artpiece_id: artpiece_id)
+                      .where.not(id: id)
+                      .where('start_date < ? AND end_date > ?', end_date, start_date)
+    errors.add(:base, 'Booking dates overlap with an existing booking') if overlaps.any?
   end
 end
