7d7a165ef274af12d2906679bdc6fd0ecb1a4ed7
Lucas Harrison <zebo200@gmail.com>
2023-06-08 11:36:42 +0100

    Backend of Dashboard bookings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @bookings = current_user.bookings
     @artpieces = current_user.artpieces
+    @all_artpieces = Artpiece.all
   end
 end
