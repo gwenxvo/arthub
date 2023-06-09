57a1dc17e3603581f878ec3d46b96d2c198bc668
Rostyslav Komar <komarostyslav@gmail.com>
2023-06-09 13:33:36 +0300

    check the user artpiece

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,6 +3,8 @@ class RatingsController < ApplicationController
     @artpiece = Artpiece.find(params[:artpiece_id])
     @rating = Rating.new(rating_params)
     @rating.artpiece = @artpiece
+    @rating.current_user = current_user
+
     if @rating.save
       redirect_to artpiece_path(@artpiece)
     else
